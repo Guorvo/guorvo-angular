98765e7ab44a4d250fd3874265b61538d2935a55
Guorvo <lmcuchero@gmail.com>
2023-11-01 18:47:16 -0300

    angular is pretty cool

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Product } from './models/product.model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'guorvo-angular';
+  http = inject(HttpClient);
+  products: Product[] = [];
+
+  changeTitle() {
+    this.title = 'changed';
+  }
+
+  ngOnInit() {
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data) => {
+        this.products = data;
+    });
+  }
 }
